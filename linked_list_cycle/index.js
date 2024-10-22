8f05a814edccf767bca265c53d6d98273b5d0b6b
arthurvenicio <arthur@justcode.dev>
2024-10-22 19:21:06 -0300

    linked_list_cycle: explain

--- linked_list_cycle/index.js
+++ linked_list_cycle/index.js
@@ -15,6 +15,7 @@ var hasCycle = function (head) {
   let slow = head;
 
   while (fast && fast.next) {
+    // to check if we've a cycle, create 2 pointer one slower and outher faster, if any moment both are the same we have a cycle.
     slow = slow.next;
     fast = fast.next.next;
     if (slow == fast) {
