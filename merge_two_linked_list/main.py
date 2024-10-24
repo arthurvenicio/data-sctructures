7f65cee1f199451d41f7602d6e1d6a84c2629381
arthurvenicio <arthur@justcode.dev>
2024-10-24 00:38:13 -0300

    fix:  change merge_two_linked_list

--- merge_two_linked_list/main.py
+++ merge_two_linked_list/main.py
@@ -15,10 +15,8 @@ class Solution:
             else:
                 curr.next = list2
                 list2 = list2.next
+            curr = curr.next
         
-        if list1:
-            curr.next = list1
-        else:
-            curr.next = list2
+        curr.next = list1 or list2
         
         return dummy.next
