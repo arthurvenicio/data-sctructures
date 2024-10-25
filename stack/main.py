193dc04d0833167c762e9612f5ac15c5f9142b98
arthurvenicio <arthur@justcode.dev>
2024-10-25 02:10:43 -0300

    stack - change default code

--- stack/main.py
+++ stack/main.py
@@ -1,15 +1,20 @@
 class Stack:
-    def __init__(self) -> None:
-        self.items = []
+    def __init__(self, max_lenght = 0) -> None:
+        self.items = [0]*max_lenght
+        self.max_lenght = max_lenght
+        self.pointer = 0
         
     def push(self, item):
-        self.items.append(item)
+        self.items[self.pointer] = item
+        self.pointer += 1
         
     def pop(self):
         if not len(self.items):
             raise IndexError("Empty list")
         
-        return self.items.pop()
+        item = self.items[-1]
+        self.pointer -=1
+        return item
     
     def peek(self):
         if not len(self.items):
@@ -17,4 +22,4 @@ class Stack:
         return self.items[-1]
     
     def size(self):
-        return len(self.items)
+        return self.pointer
