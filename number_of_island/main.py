0ff99530406b04713269cb124cf98171a31bdec5
arthurvenicio <arthur@justcode.dev>
2024-10-30 01:47:20 -0300

    number_of_island

---
+++ number_of_island/main.py
@@ -0,0 +1,12 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        return
+        
+
+obj = Solution()
+print(obj.numIslands([
+  ["1","1","1","1","0"],
+  ["1","1","0","1","0"],
+  ["1","1","0","0","0"],
+  ["0","0","0","0","0"]
+]))
