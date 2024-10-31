ea59faa5e6cc55a7d9222ffbb5e13bfe6354e826
arthurvenicio <arthur@justcode.dev>
2024-10-31 03:25:55 -0300

    contains_duplicate: another solution

--- contains_duplicate/main.py
+++ contains_duplicate/main.py
@@ -10,3 +10,11 @@ class Solution:
 
         return False
         
+        
+# another solution
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        nums_set  = set(nums)
+        
+        return True if len(nums_set) < len(nums) else False
