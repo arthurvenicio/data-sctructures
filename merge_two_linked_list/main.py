a975fedae868128827aead2f6fb33123dfd9384f
arthurvenicio <arthur@justcode.dev>
2024-11-01 00:32:42 -0300

    fix:merge_two_linked_list

--- merge_two_linked_list/main.py
+++ merge_two_linked_list/main.py
@@ -1,22 +1,37 @@
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        dummy = ListNode(-1)
-        curr = dummy
+       if not list1:
+           return list2
+       if not list2:
+           return list1
+       
+       
+       if list1.val <= list2.val:
+           list1.next = self.mergeTwoLists(list1.next, list2)
+           return list1
+       else:
+           list2.next = self.mergeTwoLists(list2.next, list1)
+           return list2
+        
+        
+sol = Solution()
 
-        while list1 and list2:
-            if list1.val < list2.val:
-                curr.next = list1
-                list1 = list1.next
-            else:
-                curr.next = list2
-                list2 = list2.next
-            curr = curr.next
-        
-        curr.next = list1 or list2
-        
-        return dummy.next
+
+nodeA3 = ListNode(4)
+nodeA2 = ListNode(2, nodeA3)
+headA1 = ListNode(1, nodeA2)
+
+
+nodeB3 = ListNode(4)
+nodeB2 = ListNode(3, nodeA3)
+headB1 = ListNode(1, nodeA2)
+
+print(sol.mergeTwoLists(headA1, headB1))
