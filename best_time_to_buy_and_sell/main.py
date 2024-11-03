35d3a63506ef5f3b8a86b2de23590e72df042a6f
arthurvenicio <arthur@justcode.dev>
2024-11-03 05:27:59 -0300

    best_time_to_buy_and_sell

--- best_time_to_buy_and_sell/main.py
+++ best_time_to_buy_and_sell/main.py
@@ -47,6 +47,25 @@ class Solution:
                 max_profit = curr_profit
             
         return max_profit
+
+## Sliding window
+
+# class Solution:
+#     def maxProfit(self, prices: List[int]) -> int:
+#         left, right = 0,1
+#         max_profit = 0
+        
+        
+#         while right < len(prices):
+#             if prices[left] < prices[right]:
+#                 current_profit = prices[right] - prices[left]
+#                 max_profit = max(max_profit, current_profit)
+#             else:
+#                 left = right
+#             right += 1
+                
+#         return max_profit
+
     
 sol = Solution()
 print(sol.maxProfit([7,1,5,3,6,4]))
