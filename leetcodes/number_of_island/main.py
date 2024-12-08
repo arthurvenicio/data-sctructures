7973bbb714bf6afa192a4400fa725c7885147242
arthurvenicio <arthur@justcode.dev>
2024-12-08 19:18:46 -0300

    number_of_island: update

--- leetcodes/number_of_island/main.py
+++ leetcodes/number_of_island/main.py
@@ -1,6 +1,42 @@
+from typing import List
+from collections import deque
+
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        return
+        if not grid:
+          return 0
+        
+        rows, cols = len(grid) , len(grid[0])
+        visit = set()
+        islands = 0
+        
+        def bfs (r,c):
+          queue = deque()
+          visit.add((r, c))
+          queue.append((r, c))
+          
+          while queue:
+            row, col = queue.popleft()
+            directions = [[1,0], [-1, 0], [0,1], [0, -1]]
+            
+            for dr, dc in directions:
+              r, c = row + dr, col + dc
+              
+              if(r in range(rows) and
+                 c in range(cols) and
+                 grid[r][c] == "1" and
+                 (r, c) not in visit):
+                
+                queue.append((r, c))
+                visit.add((r,c))
+        
+        for r in range(rows):
+          for c in range(cols):
+            if grid[r][c] == "1" and (r, c) not in visit:
+              bfs(r,c)
+              islands += 1
+        
+        return islands
         
 
 obj = Solution()
@@ -10,3 +46,6 @@ print(obj.numIslands([
   ["1","1","0","0","0"],
   ["0","0","0","0","0"]
 ]))
+
+# Method used : BFS
+# Time complexy: O(r * c)
