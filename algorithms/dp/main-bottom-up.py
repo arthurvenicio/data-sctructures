241d69aa1d56dd5367af51837b329b2034cb8fb4
arthurvenicio <arthur@justcode.dev>
2024-12-21 22:06:41 -0300

    dp: add another solution

--- algorithms/dp/main-bottom-up.py
+++ algorithms/dp/main-bottom-up.py
@@ -15,3 +15,21 @@ class Solution:
     
 # Tabulation
 # time complexity: O(n)
+# space complexity: O(n)
+
+
+class Solution:
+    def fib(self, n: int) -> int:
+        if n == 0 or n == 1:
+            return n
+        
+        prev = 0
+        curr = 1
+
+        for i in range(2, n+1):
+            prev, curr = curr, prev + curr
+
+        return curr
+    
+# Time complexity: O(n)
+# Space complexity: O(1)
