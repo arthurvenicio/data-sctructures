7b9ba64fdd241f97f26f752e4d816dc410524916
arthurvenicio <arthur@justcode.dev>
2024-12-22 03:07:00 -0300

    fix: longest_common_prefix

--- leetcodes/longest_common_prefix/main.py
+++ leetcodes/longest_common_prefix/main.py
@@ -3,14 +3,23 @@ from typing import List
 
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        res = ""
+        min_length = float('inf')
         
-        for i in range(len(strs)):
-            for string in strs:
-                if i == len(string) or string[i] != strs[0][i]:
-                    return res
-            res += strs[0][i]
-        return res
+        for s in strs:
+            if len(s) < min_length:
+                min_length = len(s)
+                
+        i = 0
+        
+        while i < min_length:
+            for s in strs:
+                if s[i] != strs[0][i]:
+                    return s[:i]            
+            i += 1
+            
+        return s[:i]
     
+    # time complexity: O(n * m)
+    # space : O(1)
 sol = Solution()
 print(sol.longestCommonPrefix(["flower","flow","flight"]))
