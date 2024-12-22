e80aa16ac47fb749e5dab9aaf0e33e781dace909
arthurvenicio <arthur@justcode.dev>
2024-12-22 20:46:28 -0300

    merge_intervals: add another solution

--- leetcodes/merge_intervals/main.py
+++ leetcodes/merge_intervals/main.py
@@ -1,15 +1,33 @@
+# class Solution:
+#     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+#         ## should sort
+#         intervals.sort(key = lambda i: i[0])
+#         output = [intervals[0]]
+
+#         for s, e in intervals:
+#             last_end = output[-1][1]
+
+#             if s <= last_end:
+#                 output[-1][1] = max(last_end, e)
+#             else:    
+#                 output.append([s, e])
+
+#         return output
+    
+    
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         ## should sort
         intervals.sort(key = lambda i: i[0])
-        output = [intervals[0]]
+        merged = []
 
-        for s, e in intervals:
-            last_end = output[-1][1]
+        for interval in intervals:
+            if not merged or merged[-1][1] < interval[0]:
+                merged.append(interval)
+            else:
+                merged[-1] = [merged[-1][0], max(merged[-1][1], interval[1])]
 
-            if s <= last_end:
-                output[-1][1] = max(last_end, e)
-            else:    
-                output.append([s, e])
-
-        return output
+        return merged
+    
+# Time complexity: O(n log n)
+# Space complexity: O(n)
